368414c7f5db16b0bc4afb232809cc70767883dd
Julien Quere <jquere@regionsjob.com>
2014-02-10 12:27:58 +0100

    Initial configuration import

---
+++ dotfiles/dotfiles.sh
@@ -0,0 +1,4 @@
+source ~/.dotfiles/path.sh
+source ~/.dotfiles/gitcompletion.sh
+source ~/.dotfiles/prompt.sh
+source ~/.dotfiles/aliases.sh
---
+++ dotfiles/prompt.sh
@@ -0,0 +1,3 @@
+export GIT_PS1_SHOWDIRTYSTATE=1 GIT_PS1_SHOWSTASHSTATE=1 GIT_PS1_SHOWUNTRACKEDFILES=1
+export GIT_PS1_SHOWUPSTREAM=verbose GIT_PS1_DESCRIBE_STYLE=branch GIT_PS1_SHOWCOLORHINTS=1
+export PS1='\[\e[0;36m\][\A] \u@\h:\[\e[0m\e[0;32m\]\W\[\e[1;33m\]$(__git_ps1 " (%s)")\[\e[0;37m\] \$\[\e[0m\] '
