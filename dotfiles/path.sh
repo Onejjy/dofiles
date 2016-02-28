48463414b2ac3b69e6b890da48935191205d2199
Julien Quéré <julien@sinplicity.fr>
2016-02-28 17:25:16 +0100

    Dotfiles update

--- dotfiles/path.sh
+++ dotfiles/path.sh
@@ -1,1 +1,3 @@
-export PATH=/usr/local/bin:$PATH
+export ANDROID_HOME=~/Library/Android/sdk 
+export GEM_HOME=$HOME/.gemhome
+export PATH=/usr/local/bin:$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$GEM_HOME/bin
